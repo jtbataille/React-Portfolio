9a374f94aa65bb9f8180ab99d0744333ce066c4c
Jarrod Bataille <jtbataille@gmail.com>
2020-11-12 13:21:29 -0500

    adding home page and some pictures

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-function Home() {
-    return null;
+import Me from '../imgs/me.jpg';
+
+const Home = () => {
+    <>
+        <div className="content">
+            
+            <div className="jumbotron text-center">
+              <h1>Jarrod Bataille</h1>
+              <h3> {Me} </h3>
+            </div>
+
+            {/* Tentative introduction regrading me and my coding "history" */}
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4 pb-3">A bit about me . . .</h1>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <img src="../imgs/jtbLogo.gif" alt="GIF of logo" width="95%" height="85%" />
+                        </div>
+
+                        <div className="col-md-8">
+                            <p className="lead">Welcome to my site! My name is Jarrod Bataille, and I am a full-stack developer.</p>
+                            
+                            <p>I was drawn into the world of web development while working as an English teacher in China, Italy, and later in the United States. After seeing the disparities among groups of students and groups of adults with internet accessibility, the need I saw there kindled in me the fire of building programs and applications aimed at aiding in web literacy and access--something I consider to be critical for web development.</p>
+                            
+                            <p>With this passion for programming in mind, I hope you will see this passion in some of the many projects I have uploaded here. Please browse through the site, and send in any inquiries using the contact form!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
 }
 
 export default Home;
