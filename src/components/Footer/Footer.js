dad17fc700187e5b833b7231bd1579b4e2b846b8
Jarrod Bataille <jtbataille@gmail.com>
2020-11-12 16:02:14 -0500

    fixing color scheme for all pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,15 +4,17 @@ import './style.css';
 
 function Footer() {
     return (
-        <div className="row">
-            <small className="copyright"> &copy; Jarrod Bataille 2020</small>
-            <ul className="social-network social-circle ml-auto">
-                <li><a href="https://www.linkedin.com/in/jarrod-bataille-9154461aa/" target="_blank" rel="noopener noreferrer" className="icoLinkedin" title="Linkedin"><i className="fa fa-linkedin"></i></a></li>
-                <li><a href="mailto:jtbataille@gmail.com" className="icoMail" title="Mail"><i className="fa fa-envelope"></i></a></li>
-                <li><a href="https://twitter.com/jtbataille" target="_blank" rel="noopener noreferrer" className="icoTwitter" title="Twitter"><i className="fa fa-twitter"></i></a></li>
-                <li><a href="https://github.com/jtbataille" target="_blank" rel="noopener noreferrer" className="icoGitHub" title="GitHub"><i className="fa fa-github"></i></a></li>
-            </ul>
-        </div>
+        <footer class="py-2 px-5 sticky-bottom">
+            <div className="row">
+                <small className="copyright"> &copy; Jarrod Bataille 2020</small>
+                <ul className="social-network social-circle ml-auto">
+                    <li><a href="https://www.linkedin.com/in/jarrod-bataille-9154461aa/" target="_blank" rel="noopener noreferrer" className="icoLinkedin" title="Linkedin"><i className="fa fa-linkedin"></i></a></li>
+                    <li><a href="mailto:jtbataille@gmail.com" className="icoMail" title="Mail"><i className="fa fa-envelope"></i></a></li>
+                    <li><a href="https://twitter.com/jtbataille" target="_blank" rel="noopener noreferrer" className="icoTwitter" title="Twitter"><i className="fa fa-twitter"></i></a></li>
+                    <li><a href="https://github.com/jtbataille" target="_blank" rel="noopener noreferrer" className="icoGitHub" title="GitHub"><i className="fa fa-github"></i></a></li>
+                </ul>
+            </div>
+        </footer>
     )
 }
 
